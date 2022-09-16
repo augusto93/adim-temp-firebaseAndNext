779c8c8cdcc916d800e6e0de0d79217a8a197efc
augusto93 <marceloaugusto9365@gmail.com>
2022-09-16 17:26:19 -0300

    fix 1

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -11,8 +11,11 @@ export default function AvatarUsuario(props: AvatarUsuarioProps) {
   return (
     <Link href="/perfil">
       <Image 
-        src={usuario?.imagemUrl ?? '/images/avatar.svg'} 
+        // src={usuario?.imagemUrl ?? '/images/avatar.svg'} 
+        src='/images/avatar.svg'
         alt="Avatar do Usuário" 
+        width="40"
+        height="40"
         className={`
           h-10 w-10 rounded-full cursor-pointer
           ${props.className}
